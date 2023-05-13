afe1216d3a450692b333c9ece36ed6d5afea1198
natanmoura <natanmoura@escavador.com>
2023-05-13 13:58:38 -0300

    novas tentativas

--- human_detection.py
+++ human_detection.py
@@ -1,21 +1,43 @@
-from ultralytics import YOLO
 import cv2
+import glob
+import os
 
-# Carrega o modelo yolov8x
-model = YOLO("yolov8x.pt")
+from ultralytics import YOLO
 
-# Lê a imagem de fogo previamente selecionada
-image = cv2.imread("./images/4400060234.jpg")
 
-# Prediz a classe 0 (person) na imagem anterior, salvando tanto imagem anotada quanto posição
-results = model.predict(source=image, classes=0)
+def detect_class_inside_dataset(source_path: str, dest_path: str, original_index: int, final_index: int):
+    """
+    Detecta uma classe específica num conjunto de imagens definidas em source_path, salvando suas labels referentes
+    em dest_path, as labels terão o mesmo nome que as imagens. (Evita detectar imagens que já possuem lables em
+    dest_path)
 
-predicitons_list = []
-for box in results[0].boxes:
-    positions = box.xywhn.tolist()[0]
-    predicitons_list.append(
-        f"8 {' '.join(str('%.7f' % p) for p in positions)}"
-    )
+    :param source_path: Path de origem das imagens.
+    :param dest_path: Path de destino da label das imagens.
+    :param original_index: Index original da classe.
+    :param final_index: Index final da classe (que será salvo nas labels).
+    """
+    model = YOLO("yolov8x.pt")
 
-with open("./predicted_labels/4400060234.txt", "w") as file:
-    file.write("\n".join(predicitons_list))
+    os.chdir(dest_path)
+
+    already_done = [os.path.basename(txt) for txt in glob.glob("*.txt")]
+
+    os.chdir(source_path)
+
+    for image_path in glob.glob("*.jpe?g"):
+
+        image = cv2.imread(image_path)
+
+        results = model.predict(source=image, classes=original_index)
+
+        predicitons_list = []
+        for box in results[0].boxes:
+            positions = box.xywhn.tolist()[0]
+            predicitons_list.append(f"{final_index} {' '.join(str('%.7f' % p) for p in positions)}")
+
+        dest_path = dest_path + "/" if dest_path[-1] != "/" else dest_path
+        with open(dest_path + os.path.basename(image_path), "w") as file:
+            file.write("\n".join(predicitons_list))
+
+
+detect_class_inside_dataset("./images", "./predicted_labels", 0, 8)
